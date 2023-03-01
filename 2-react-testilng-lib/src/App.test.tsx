731700d7f9681b71a2bcc2bd51a334acb6750b3a
gapur <kaloycant@gmail.com>
2023-03-01 23:21:58 +0100

    findBy, getBy, queryBy. example with useEffect. async code. events - onClick, onChange, onInput. FireEvent, userEvent

--- 2-react-testilng-lib/src/App.test.tsx
+++ 2-react-testilng-lib/src/App.test.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("TEST APP RTL", () => {
-  test('renders learn react link', () => {
+  test("renders learn react link", () => {
     render(<App />);
     const helloWorldElement = screen.getByText(/hello world!/i);
-    const btnElement = screen.getByRole('button');
+    const btnElement = screen.getByRole("button");
     const inputElement = screen.getByPlaceholderText(/input value/i);
     expect(helloWorldElement).toBeInTheDocument();
     expect(btnElement).toBeInTheDocument();
     expect(inputElement).toMatchSnapshot();
+  });
+
+  test("learning find..., get..., query...", async () => {
+    render(<App />);
+
+    // // example with queryBy
+    // const helloWorldElement = screen.queryByText(/hello2/i);
+    // expect(helloWorldElement).toBeNull()
+
+    // example with findBy
+    const helloWorldElement = await screen.findByText(/data/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(helloWorldElement).toHaveStyle({ color: "red" });
+  });
+
+  test("CLICK EVENT", () => {
+    render(<App />);
+    const btn = screen.getByTestId("toggle-button");
+
+    expect(screen.queryByTestId("toggle-elem")).toBeNull();
+    fireEvent.click(btn);
+    expect(screen.getByTestId("toggle-elem")).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId("toggle-elem")).toBeNull();
+  });
+
+  test("INPUT EVENT", () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/input value/i);
+
+    expect(screen.queryByTestId("value-title")).toHaveTextContent("");
+    // fireEvent.input(input, {
+    //   target: { value: "123123" },
+    // });
+
+    // userEvent closer to user behavior 
+    userEvent.type(input, '123123');
+    expect(screen.queryByTestId("value-title")).toHaveTextContent("123123");
   });
 });
--- 2-react-testilng-lib/src/App.tsx
+++ 2-react-testilng-lib/src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState<null | {}>(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onClick = () => setToggle(prev => !prev)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 100);
+  }, []);
+
   return (
     <div className="App">
+      <h1 data-testid="value-title">{value}</h1>
+      {toggle && (
+        <div data-testid="toggle-elem">toggle</div>
+      )}
+      {data && (
+        <div style={{color: 'red'}}>data</div>
+      )}
       <h1>Hello world!</h1>
-      <button>Click me</button>
-      <input type="text" placeholder="input value...." />
+      <button data-testid="toggle-button" onClick={onClick}>Click me</button>
+      <input onChange={(e) => setValue(e.target.value)} type="text" placeholder="input value...." />
     </div>
   );
 }
